13b5da253c938011a87c2c03b3667b4a9eebea18
Sally Chhuon <schhuo2@gmail.com>
2020-03-18 15:41:44 -0500

    First commit to Github

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
 
+    let resultString = "";
+
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    for (x = 0; x < num; x++) {
+        resultString += word;
+    }    
+    return resultString;
 }
 
 module.exports = repeatString

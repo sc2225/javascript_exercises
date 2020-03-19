373eb81438f5b9cc4d40c56e7523391e7f407c7e
Sally Chhuon <schhuo2@gmail.com>
2020-03-18 22:32:52 -0500

    Final Solution Submission

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+
+    //NOTE: Arguments.length takes into account ALL arguments/parameters in the parantheses
+
+    //use array.indexOf("X") to find position of a value in the array
+    //Use arr.splice(index, #toRemove) to remove the value
+
+   
+
+    //To determine if multiple optional arguments are present, get the number of optional arguments
+    //and loop through them
+
+    if (arguments.length - 1 > 0) {
+        for (argCounter = 1; argCounter <= (arguments.length - 1); argCounter++) {
+            let value = arguments[argCounter];
+
+            let itemFound = arr.indexOf(value);
+            if (itemFound != -1) {
+                arr.splice(itemFound, 1);
+            }
+            
+        }
+    }
+
+    return arr;
 
 }
 
+console.log(removeFromArray([1,2,3,4], 2));
 module.exports = removeFromArray

99399d493472a88ee8cd563c0ee74d23233bb5d6
Sally Chhuon <schhuo2@gmail.com>
2020-03-24 15:24:44 -0500

    Final solutions

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const ftoc = function() {
+const ftoc = function(farenheit) {
+
+  let celsius = (farenheit - 32) * (5/9);
+  celsius = Math.round(celsius*10)/10;
+
+  return celsius;
 
 }
 
-const ctof = function() {
+const ctof = function(celsius) {
+  let farenheit = celsius * (9/5) + 32;
+  farenheit = Math.round(farenheit*10)/10;
+  return farenheit;
 
 }
 

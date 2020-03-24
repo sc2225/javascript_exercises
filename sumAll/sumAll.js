6529c384bc0d770a4f3a405fb916670b1263e742
Sally Chhuon <schhuo2@gmail.com>
2020-03-24 14:58:14 -0500

    final solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let results = 0;
+    let thisStart= start;
+    let thisEnd = end;
+
+    //first check if parameters are numbers. If ONE of the para are not, then return error.
+
+    if (typeof start != 'number' || typeof end != 'number' || start < 0 || end < 0) {
+        return 'ERROR';
+    }
+
+    if (start >= end) {
+        thisStart = end;
+        thisEnd = start;
+    }
+
+    while (thisStart <= thisEnd) {
+    
+        results += thisStart;
+        thisStart++;
+        
+    }
+
+    
+return results;
 
 }
 
